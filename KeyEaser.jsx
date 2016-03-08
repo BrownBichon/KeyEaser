cc88e83cd4aa316b59af6aa5618fcc0da7a98fce
BrownBichon <brownbichon@hotmail.com>
2016-03-08 19:40:19 +0800

    Adding Main Script & Test Project

---
+++ KeyEaser.jsx
@@ -0,0 +1,18 @@
+﻿app.beginUndoGroup("Keyframe");
+var myComps = app.project.activeItem;
+var myLayers = myComps.selectedLayers;
+var myProps = myLayers[0].selectedProperties;
+var myKeys = myProps[0].selectedKeys;
+
+//myKey = new KeyframeEase(speed, influence);
+var easeSpeed = 50;
+var easeInfluence = 50;
+var myEase = new KeyframeEase(easeSpeed, easeInfluence);
+
+
+myProps[0].setTemporalEaseAtKey(myKeys[1], [myEase],
+[myEase]);
+
+alert(myProps[0].keyValue(myKeys[1]));
+
+app.endUndoGroup();
