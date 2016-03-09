64964c44be4e28fc93a235b56a023d7969381027
BrownBichon <brownbichon@hotmail.com>
2016-03-09 13:28:29 +0800

    Add endKey

--- KeyEaser.jsx
+++ KeyEaser.jsx
@@ -10,7 +10,17 @@ var easeInfluence = 50;
 var myEase = new KeyframeEase(easeSpeed, easeInfluence);
 
 
-myProps[0].setTemporalEaseAtKey(myKeys[1], [myEase],
-[myEase]);
+// myProps[0].setTemporalEaseAtKey(myKeys[1], [myEase],
+// [myEase]);
+
+// setValueAtTime(time, newValue)
+// keyTime(keyIndex)
+// keyValue(keyIndex)
+
+
+//Add endKey
+var endTime = 2*myProps[0].keyTime(myKeys[1])-myProps[0].keyTime(myKeys[0]);
+var endValue = myProps[0].keyValue(myKeys[1]);
+myProps[0].setValueAtTime(endTime, endValue);
 
 app.endUndoGroup();
